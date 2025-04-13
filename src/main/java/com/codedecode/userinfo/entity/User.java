572d91c95d9cd2e65661646f3686b0c2ac91a345
githubrepositoryuser <35899078+githubrepositoryuser@users.noreply.github.com>
2025-04-13 19:47:24 +0530

    Update User.java

--- src/main/java/com/codedecode/userinfo/entity/User.java
+++ src/main/java/com/codedecode/userinfo/entity/User.java
@@ -8,11 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
